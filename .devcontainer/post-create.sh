7645fc3182ce75691f5b82558ba61400ce34b700
Hitesh Nayak <hiteshnayak305@gmail.com>
2025-10-03 21:19:24 +0000

    chore(dev): upgrade ansible version and dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,10 @@
 echo "Running post-create commands..."
 # Add your setup commands below
 
-PACKAGE="ansible"
+ANSIBLE_VERSION="12.0"
+ANSIBLE_LINT_VERSION="25.9"
+YAMLLINT_VERSION="1.37"
 PIP_ARGS="--no-cache-dir --force-reinstall"
 
-pipx install --include-deps --force --pip-args "$PIP_ARGS" "$PACKAGE"
-pipx inject  --include-apps --include-deps --force --pip-args "$PIP_ARGS" "$PACKAGE" ansible-lint yamllint
+pipx install --include-deps --force --pip-args "$PIP_ARGS" "ansible~=$ANSIBLE_VERSION"
+pipx inject  --include-apps --include-deps --force --pip-args "$PIP_ARGS" "ansible" "ansible-lint~=$ANSIBLE_LINT_VERSION" "yamllint~=$YAMLLINT_VERSION"
